664c7df1b62aadf8a8daf870b4c81b73b332918a
Sunil Pandey <mailtosunil.cse@gmail.com>
2017-06-20 22:15:09 +0530

    2nd commit

--- src/org/pandey/sunil/DrawingApp.java
+++ src/org/pandey/sunil/DrawingApp.java
@@ -2,15 +2,19 @@ package org.pandey.sunil;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 public class DrawingApp {
 
 	public static void main(String[] args) {
 		
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		
-	   	Triangle triangle = (Triangle) factory.getBean("triangle");
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+				
+	   	//Triangle triangle = (Triangle) factory.getBean("triangle");
+		Triangle triangle = (Triangle) context.getBean("triangle");
 	   	
 	   	triangle.draw();
 
